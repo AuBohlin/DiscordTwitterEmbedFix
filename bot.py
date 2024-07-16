ceb5e7685071f5f7a545326f2b90902217d76c4d
AuBohlin <43729544+AuBohlin@users.noreply.github.com>
2024-07-15 22:13:26 -0400

    Added the fxtwitter functionality

--- bot.py
+++ bot.py
@@ -9,5 +9,30 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-client = discord.Client()
-bot = commands.Bot(command_prefix="`")
+#new intents needed
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='?', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.content.startswith('https://x.com') or message.content.startswith('https://twitter.com'):
+        fix = message.content.replace('x.com', 'fxtwitter.com')
+        
+        name = message.author.nick
+        if name is None:
+            name = message.author.global_name
+        msg = f'{fix} \nFrom: {name}'
+        await message.delete()
+        await message.channel.send(msg)
+        
+
+bot.run(TOKEN)
